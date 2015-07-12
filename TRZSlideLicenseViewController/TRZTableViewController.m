94e116dd3d924de512923545c348886a07bea8b5
86 <triaedz@gmail.com>
2015-07-12 23:57:14 +0900

    recreate Main.storyboard

--- TRZSlideLicenseViewController/TRZTableViewController.m
+++ TRZSlideLicenseViewController/TRZTableViewController.m
@@ -35,21 +35,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    // Return the number of sections.
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    // Return the number of rows in the section.
-    return 4;
-}
-
-
 #pragma mark - Navigation
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -100,6 +85,5 @@
         controller.textColor = [UIColor lightGrayColor];
     }
 }
-
 
 @end
